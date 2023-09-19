74d38bd28f3c909b3ab4a51d12e2af95bb175a9c
Dermot Bateman <dermotbateman@gmail.com>
2023-09-19 14:37:41 +0300

    done: 4.9

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -5,12 +5,19 @@ const api = supertest(app)
 // const Blog = require('../models/blog')
 
 
-test('return all notes', async () => {
+test('return all blogs', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/)
 }, 10000)
+
+test('verify blogs id is defined id', async () => {
+  const resp = await api.get('/api/blogs')
+  resp.body.forEach( (r) => {
+    expect(r.id).toBeDefined()
+  })
+})
 
 afterAll(async () => {
   await mongoose.connection.close()
