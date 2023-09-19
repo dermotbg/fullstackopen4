59776ddfdb0e220002d8606427f7db57da9c8717
Dermot Bateman <dermotbateman@gmail.com>
2023-09-19 15:13:00 +0300

    done: 4.10

--- bloglist/tests/blog_api.test.js
+++ bloglist/tests/blog_api.test.js
@@ -19,6 +19,27 @@ test('verify blogs id is defined id', async () => {
   })
 })
 
+test('a valid blog can be added', async () => {
+  const newBlog = {
+    title: 'Blog created in test',
+    author: 'Dermot',
+    url: '/test/blog',
+    likes: 7
+  }
+  const blogsAtStart = await api.get('/api/blogs')
+
+  await api
+    .post('/api/blogs')
+    .send(newBlog)
+    .expect(201)
+    .expect('Content-Type', /application\/json/)
+
+  const response = await api.get('/api/blogs')
+  expect(response.body).toHaveLength(blogsAtStart.body.length + 1)
+  const titles = response.body.map(r => r.title)
+  expect(titles).toContain('Blog created in test')
+})
+
 afterAll(async () => {
   await mongoose.connection.close()
 })
