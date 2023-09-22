d9c6e97a0c4847dd075ae9b93d517ee65f4ec26b
Dermot Bateman <dermotbateman@gmail.com>
2023-09-22 13:25:54 +0300

    done: 4.17

--- bloglist/controllers/blogs.js
+++ bloglist/controllers/blogs.js
@@ -1,8 +1,10 @@
 const blogsRouter = require('express').Router()
 const Blog = require('../models/blog')
+const User = require('../models/user')
 
 blogsRouter.get('/', async (_request, response) => {
   const blogs = await Blog.find({})
+    .populate('user', { username: 1, name: 1, id: 1 })
   response.json(blogs)
 })
 
@@ -22,13 +24,21 @@ blogsRouter.post('/', async (request, response) => {
     response.status(400).end()
   }
   else {
+
+    const user = await User.findById(body.userId)
+
     const blog = new Blog({
       title: body.title,
       author: body.author,
       url: body.url,
-      likes: body.likes ? body.likes : 0
+      likes: body.likes ? body.likes : 0,
+      user: user.id
     })
+
     const savedBlog = await blog.save()
+    user.blogs = user.blogs.concat(savedBlog._id)
+
+    await user.save()
     response.status(201).send(savedBlog)
   }
 })
